9f6d3f3e13af729c071cd50d7710c2f499812d28
hyunwook990 <jho99061288@gmail.com>
2025-05-14 20:01:29 +0900

    absolute positioning

---
+++ PyQt5/layout/Absolute.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QDesktopWidget
+
+class MyApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        label1 = QLabel('Label1', self)
+        label1.move(20, 20)
+        label2 = QLabel('Label2', self)
+        label2.move(20, 60)
+
+        btn1 = QPushButton('Button1', self)
+        btn1.move(80, 13)
+        btn2 = QPushButton('Button2', self)
+        btn2.move(80, 53)
+
+        self.setWindowTitle("Layout")
+        self.resize(500, 300)
+        self.center()
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
