1207ece42078676c549311ec55416a3a85af8547
hyunwook990 <jho99061288@gmail.com>
2025-05-14 19:13:00 +0900

    window locate to center

--- before_garbage_project/PyQt5/QMainWindow.py
+++ before_garbage_project/PyQt5/QMainWindow.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp, QDesktopWidget
 from PyQt5.QtGui import QIcon
 
 class MyApp(QMainWindow):
@@ -20,8 +20,16 @@ class MyApp(QMainWindow):
         self.toolbar.addAction(exitAction)  # 툴바에 동작 추가
 
         self.setWindowTitle("MyWindow")
-        self.setGeometry(300, 300, 400, 200)
+        # self.setGeometry(300, 300, 400, 200)
+        self.resize(500, 350)   # 창 크기 설정
+        self.center()   # center 함수 실행
         self.show()
+
+    def center(self):
+        qr = self.frameGeometry()   # 창의 위치, 크기 정보를 가져온다.
+        cp = QDesktopWidget().availableGeometry().center()  # 모니터의 가운데를 파악
+        qr.moveCenter(cp)   # 모니터의 중심점으로 창의 중심점을 이동한다.
+        self.move(qr.topLeft()) # 현재 창을 화면의 중심으로 이동했던 직사각형(qr)에 이동 (실제 이동 작업)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
