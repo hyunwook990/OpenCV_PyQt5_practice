a03001f3f3bc4eb3f37cc7a3a5ddbaf85bf183ae
hyunwook990 <jho99061288@gmail.com>
2025-05-14 18:32:44 +0900

    ToolBar

--- before_garbage_project/PyQt5/QMainWindow.py
+++ before_garbage_project/PyQt5/QMainWindow.py
@@ -1,14 +1,24 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp
+from PyQt5.QtGui import QIcon
 
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.InitUI()
+        self.initUI()
     
-    def InitUI(self):
-        self.statusBar().showMessage("Ready")
+    def initUI(self):
+        exitAction = QAction(QIcon('exit.png'), 'Exit', self)   # 객체 생성
+        exitAction.setShortcut('Ctrl+Q')    # 단축키 설정
+        exitAction.setStatusTip('Exit application') # statusBar에 표시할 텍스트
+        exitAction.triggered.connect(qApp.quit) # 동작 설정
+
+        # statusBar()는 QWidget에는 없고 QMainWindow에만 있다.
+        self.statusBar().showMessage("Ready")   # 객체 생성 및 처음 나오는 text설정
         
+        self.toolbar = self.addToolBar('Exit')  # 툴바 생성
+        self.toolbar.addAction(exitAction)  # 툴바에 동작 추가
+
         self.setWindowTitle("MyWindow")
         self.setGeometry(300, 300, 400, 200)
         self.show()
