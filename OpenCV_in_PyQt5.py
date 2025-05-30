ab0afeb7ff8e2529854c952523e10b45089c92ca
hyunwook990 <jho99061288@gmail.com>
2025-05-30 19:42:01 +0900

    show image with current window(1차 완성)

--- OpenCV_in_PyQt5.py
+++ OpenCV_in_PyQt5.py
@@ -7,15 +7,26 @@ import cv2
 class PracticeApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.show_btn = QPushButton("New_Window")
-        self.show_btn.resize(self.show_btn.sizeHint())
-        self.show_btn.clicked.connect(self.show_image)
+        self.btn1 = QPushButton("New_Window")
+        self.btn1.resize(self.btn1.sizeHint())
+        self.btn1.clicked.connect(self.new_window)
+
+        self.btn2 = QPushButton("This_Window")
+        # self.btn2.resize(self.btn2.sizeHint())
+        self.btn2.clicked.connect(self.this_window)
+        self.btn2_temp = 0
+
+        
+        self.image = cv2.imread("study\\data\\bom.jpg", cv2.IMREAD_COLOR)
+        self.pixmap = QPixmap("study\\data\\bom.jpg")
+        self.label = QLabel()
 
         self.grid = QGridLayout()
         self.grid.addWidget(QLabel("이미지를 새 창에 띄우기: "), 0, 0)
-        self.grid.addWidget(self.show_btn, 0, 1)
+        self.grid.addWidget(self.btn1, 0, 1)
         self.grid.addWidget(QLabel("이미지를 현재 창에 띄우기: "), 1, 0)
-
+        self.grid.addWidget(self.btn2, 1, 1)
+        self.grid.addWidget(self.label, 2, 1)
 
         self.widget = QWidget()
         self.widget.setLayout(self.grid)
@@ -25,12 +36,17 @@ class PracticeApp(QMainWindow):
         self.move(300, 300)
         self.show()
 
-    def show_image(self):
-        image = cv2.imread("study\\data\\bom.jpg", cv2.IMREAD_COLOR)
-        cv2.imshow("image", image)
+    def new_window(self):
+        cv2.imshow("image", self.image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-
+    
+    def this_window(self):
+        self.btn2_temp = not self.btn2_temp
+        if self.btn2_temp:
+            self.label.setPixmap(self.pixmap)
+        else:
+            self.label.hide()
 
 
 if __name__ == "__main__":
