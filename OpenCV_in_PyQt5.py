cbc0e2da703e5f37ca9118088520f97a85bc2223
hyunwook990 <jho99061288@gmail.com>
2025-05-30 19:06:27 +0900

    show image with newwindow

--- OpenCV_in_PyQt5.py
+++ OpenCV_in_PyQt5.py
@@ -1,24 +1,35 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QGridLayout, QPushButton, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+import cv2
 
 class PracticeApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.Unit()
+        self.show_btn = QPushButton("New_Window")
+        self.show_btn.resize(self.show_btn.sizeHint())
+        self.show_btn.clicked.connect(self.show_image)
 
-    def Unit(self):
-        show_btn = QPushButton("Show", self)
-        show_btn.resize(show_btn.sizeHint())
-        grid = QGridLayout()
-        grid.addWidget(QLabel("그림 생성 버튼: "), 0, 0)
-        grid.addWidget(show_btn, 0, 1)
-        widget = QWidget()
-        widget.setLayout(grid)
-        self.setCentralWidget(widget)
+        self.grid = QGridLayout()
+        self.grid.addWidget(QLabel("이미지를 새 창에 띄우기: "), 0, 0)
+        self.grid.addWidget(self.show_btn, 0, 1)
+        self.grid.addWidget(QLabel("이미지를 현재 창에 띄우기: "), 1, 0)
+
+
+        self.widget = QWidget()
+        self.widget.setLayout(self.grid)
+        self.setCentralWidget(self.widget)
 
         self.setWindowTitle("MyWindow")
-        self.setGeometry(200, 300, 200, 300)
+        self.move(300, 300)
         self.show()
+
+    def show_image(self):
+        image = cv2.imread("study\\data\\bom.jpg", cv2.IMREAD_COLOR)
+        cv2.imshow("image", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 
