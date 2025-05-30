54d3e8039f9f4cd2018874ec0a23236df19385a4
hyunwook990 <jho99061288@gmail.com>
2025-05-30 15:28:49 +0900

    add label, pushbutton

--- PyQt5+OpenCV.py
+++ PyQt5+OpenCV.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QGridLayout, QPushButton
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QGridLayout, QPushButton, QLabel
 
 class PracticeApp(QMainWindow):
     def __init__(self):
@@ -8,8 +8,18 @@ class PracticeApp(QMainWindow):
 
     def Unit(self):
         show_btn = QPushButton("Show", self)
+        show_btn.resize(show_btn.sizeHint())
         grid = QGridLayout()
+        grid.addWidget(QLabel("그림 생성 버튼: "), 0, 0)
+        grid.addWidget(show_btn, 0, 1)
         widget = QWidget()
+        widget.setLayout(grid)
+        self.setCentralWidget(widget)
+
+        self.setWindowTitle("MyWindow")
+        self.setGeometry(200, 300, 200, 300)
+        self.show()
+
 
 
 if __name__ == "__main__":
