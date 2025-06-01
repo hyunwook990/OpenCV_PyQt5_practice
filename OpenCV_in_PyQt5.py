91fdf772ffaa8e0ca4b36122b4968e282d85e63c
정현욱 <160225759+hyunwook990@users.noreply.github.com>
2025-06-01 16:16:41 +0900

    Video with new window

--- OpenCV_in_PyQt5.py
+++ OpenCV_in_PyQt5.py
@@ -3,6 +3,11 @@ from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QGridLayout, QPu
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 import cv2
+import os
+
+print("########################################################")
+print(os.getcwd())
+print("########################################################")
 
 class PracticeApp(QMainWindow):
     def __init__(self):
@@ -16,6 +21,9 @@ class PracticeApp(QMainWindow):
         self.btn2.resize(self.btn2.sizeHint())
         self.btn2.clicked.connect(self.this_window)
         self.btn2_temp = 0
+
+        self.btn3 = QPushButton("New_Window_Video")
+        self.btn3.clicked.connect(self.new_window_video)
 
         # image 불러오기
         self.image = cv2.imread("study\\data\\bom.jpg", cv2.IMREAD_COLOR)
@@ -32,6 +40,8 @@ class PracticeApp(QMainWindow):
         self.grid.addWidget(QLabel("이미지를 현재 창에 띄우기: "), 1, 0)
         self.grid.addWidget(self.btn2, 1, 1)
         self.grid.addWidget(self.label, 2, 1)
+        self.grid.addWidget(QLabel("비디오를 새 창에 띄우기: "), 3, 0)
+        self.grid.addWidget(self.btn3, 3, 1)
 
         # widget 배치
         self.widget = QWidget()
@@ -55,6 +65,20 @@ class PracticeApp(QMainWindow):
             self.label.show()
         else:
             self.label.hide()
+
+    # 새 창에 비디오 띄우기
+    def new_window_video(self):
+        video = cv2.VideoCapture("study\\data\\video.mp4")
+
+        while video.isOpened():
+            ret, frame = video.read()
+            if not ret:
+                break
+            cv2.imshow("New Window for Video", frame)
+            if cv2.keyWait(25) == ord('q'):
+                break
+        video.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
